0df13bdd8fcde835073e9ffd0fd06b18348308b5
Mario <netrix4@gmail.com>
2026-02-27 21:58:52 -0800

    Add first visual components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import { GETProduct } from "./service/productService";
+import { GETProducts } from "./service/productService";
+import HomeContent from "./views/homeContent";
 
 export default async function Home() {
-  const response = await GETProduct();
+  // const response = await GETProduct();
 
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        {/* {JSON.stringify(response)} */}
-        
-      </main>
-    </div>
-  );
+  return <HomeContent />;
 }
